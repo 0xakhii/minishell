4dd08541e9a883ee91725481e1addd9d5c91acaa
Omar Jamal <ojamal@e2r12p1.1337.ma>
2023-06-10 05:45:35 +0100

    starts the command table

--- command_table.c
+++ command_table.c
@@ -6,8 +6,24 @@
 /*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:28:27 by ojamal            #+#    #+#             */
-/*   Updated: 2023/06/10 00:11:17 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/06/10 05:45:13 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_cmd	*create_command_table(t_tokens *lexer)
+{
+	int cmd_count;
+	t_cmd *cmd_table;
+	t_tokens *current_token = lexer;
+
+	cmd_count = 0;
+	while (current_token)
+	{
+		if (current_token->e_types == 1)
+			cmd_count++;
+		current_token = current_token->next;
+	}
+	cmd_table = malloc(sizeof(t_cmd) * cmd_count);
+}
