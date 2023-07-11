899e5cb1e4f37033ef25933d4495c4c667b84a13
Omar Jamal <ojamal@e1r10p12.1337.ma>
2023-07-11 18:46:43 +0100

    ambigous redirect fixed

--- command_table.c
+++ command_table.c
@@ -6,7 +6,7 @@
 /*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:28:27 by ojamal            #+#    #+#             */
-/*   Updated: 2023/07/11 17:06:34 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/07/11 18:43:58 by ojamal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,44 +25,110 @@ t_cmd	*table_init(void)
 	new_cmd->pipe = 0;
 	new_cmd->in_fd = -2;
 	new_cmd->out_fd = -2;
-	//new_cmd->exit_status=0;
 	return (new_cmd);
 }
 
-t_tokens	*in_files(t_cmd **new_cmd, t_tokens *current_token)
+char	*get_file_name(char *file_name, t_env_node *env)
 {
+	char	**str;
+	char	*tmp;
+
+	str = new_expand(file_name, env, 2);
+	if (!str || !str[0] || str[1])
+	{
+		ft_freeeeee(str);
+		msg_er("ambiguous redirect");
+		return (NULL);
+	}
+	tmp = ft_strdup(str[0]);
+	ft_freeeeee(str);
+	return (tmp);
+}
+
+t_tokens	*in_files(t_cmd **new_cmd, t_tokens *current_token, t_env_node *env)
+{
+	char	*file_name;
+
 	if (current_token->next && current_token->e_types == T_IN_RD)
 	{
 		current_token = current_token->next;
-		(*new_cmd)->in_fd = open(current_token->val, O_RDONLY , 0644);
+		if ((*new_cmd)->in_fd == -1 || (*new_cmd)->out_fd == -1)
+			return (current_token);
+		if ((*new_cmd)->in_fd)
+			close((*new_cmd)->in_fd);
+		file_name = get_file_name(current_token->val, env);
+		if (!file_name)
+		{
+			(*new_cmd)->in_fd = -1;
+			return (current_token);
+		}
+		(*new_cmd)->in_fd = open(file_name, O_RDONLY, 0644);
+		if ((*new_cmd)->in_fd == -1)
+			perror(file_name);
 	}
-	// else if (current_token->next && current_token->e_types == T_HERD)
-	// {
-	// 	current_token = current_token->next;
-	// 	(*new_cmd)->in_file = ft_strdup(current_token->val);
-	// }
-	return current_token;
+	else if (current_token->next && current_token->e_types == T_HERD)
+	{
+		current_token = current_token->next;
+		if ((*new_cmd)->in_fd == -1 || (*new_cmd)->out_fd == -1)
+			return (current_token);
+		if ((*new_cmd)->in_fd)
+			close((*new_cmd)->in_fd);
+		(*new_cmd)->in_file = ft_strdup(current_token->val);
+		if ((*new_cmd)->in_fd == -1)
+			perror(current_token->val);
+	}
+	return (current_token);
 }
 
-t_tokens	*out_files(t_cmd **new_cmd, t_tokens *current_token)
+t_tokens	*out_files(t_cmd **new_cmd, t_tokens *current_token,
+		t_env_node *env)
 {
+	char	*file_name;
+
 	if (current_token->next && current_token->e_types == T_OUT_RD)
 	{
 		current_token = current_token->next;
-		(*new_cmd)->out_fd = open(current_token->val, O_WRONLY | O_CREAT, 0644);
+		if ((*new_cmd)->in_fd == -1 || (*new_cmd)->out_fd == -1)
+			return (current_token);
+		if ((*new_cmd)->out_fd)
+			close((*new_cmd)->out_fd);
+		file_name = get_file_name(current_token->val, env);
+		if (!file_name)
+		{
+			(*new_cmd)->out_fd = -1;
+			return (current_token);
+		}
+		(*new_cmd)->out_fd = open(file_name, O_WRONLY | O_CREAT | O_TRUNC,
+				0644);
+		if ((*new_cmd)->out_fd == -1)
+			perror(file_name);
 	}
 	else if (current_token->next && current_token->e_types == T_APP)
 	{
 		current_token = current_token->next;
-		(*new_cmd)->out_fd = open(current_token->val, O_RDWR | O_APPEND, 0644);
+		if ((*new_cmd)->in_fd == -1 || (*new_cmd)->out_fd == -1)
+			return (current_token);
+		if ((*new_cmd)->out_fd)
+			close((*new_cmd)->out_fd);
+		file_name = get_file_name(current_token->val, env);
+		if (!file_name)
+		{
+			(*new_cmd)->out_fd = -1;
+			return (current_token);
+		}
+		(*new_cmd)->out_fd = open(file_name, O_WRONLY | O_CREAT | O_APPEND,
+				0644);
+		if ((*new_cmd)->out_fd == -1)
+			perror(file_name);
 	}
-	return current_token;
+	return (current_token);
 }
 
-t_tokens	*files_process(t_cmd **new_cmd, t_tokens *current_token)
+t_tokens	*files_process(t_cmd **new_cmd, t_tokens *current_token,
+		t_env_node *env)
 {
-	current_token = in_files(new_cmd, current_token);
-	current_token = out_files(new_cmd, current_token);
+	current_token = in_files(new_cmd, current_token, env);
+	current_token = out_files(new_cmd, current_token, env);
 	if (current_token->e_types == T_PIPE)
 	{
 		(*new_cmd)->pipe++;
@@ -100,15 +166,15 @@ char	**ft_arrjoin(char **split, char *str)
 		new_split[i] = ft_strdup(str);
 		new_split[i + 1] = NULL;
 	}
-	//ft_freeeeee(split);
 	return (new_split);
 }
 
-void	cmd_process(t_tokens *current_token, t_cmd **new_cmd, t_env_node *env, int flag)
+void	cmd_process(t_tokens *current_token, t_cmd **new_cmd, t_env_node *env,
+		int flag)
 {
 	int		i;
 	char	**str;
-	char **tmp;
+	char	**tmp;
 
 	if (current_token->e_types == T_STR)
 	{
@@ -139,7 +205,7 @@ t_cmd	*create_command_table(t_tokens *lexer, t_env_node *env)
 	while (current_token)
 	{
 		cmd_process(current_token, &new_cmd, env, 2);
-		current_token = files_process(&new_cmd, current_token);
+		current_token = files_process(&new_cmd, current_token, env);
 		current_token = current_token->next;
 	}
 	g_helper.exit_status = 0;
