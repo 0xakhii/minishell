bbdb671bf2dcd3a5193f51d29a87a94856698c60
omar jamal <ojamal@student.1337.ma>
2023-07-18 18:51:17 +0100

    Main2 (#36)
    * fixed echo && ready to push
    
    * fixed echo && ready to push
    
    * fixed echo && ready to push
    
    ---------
    
    Co-authored-by: Youssef Menyouby <ymenyoub@e1r5p6.1337.ma>

--- exec/builtins/echo.c
+++ exec/builtins/echo.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: ymenyoub <ymenyoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 01:15:59 by ymenyoub          #+#    #+#             */
-/*   Updated: 2023/06/28 08:07:34 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/07/18 02:32:42 by ymenyoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+void	print_fd(t_cmd *cmd, char *s)
+{
+	if (cmd->out_fd != -2)
+		ft_putstr_fd(s, cmd->out_fd);
+	else
+		ft_putstr_fd(s, 1);
+}
 
 int	check_option(char *s)
 {
@@ -30,7 +38,7 @@ int	check_option(char *s)
 	return (0);
 }
 
-void	echo_with_option(char **str, int *i)
+void	echo_with_option(t_cmd *cmd, char **str, int *i)
 {
 	while (str[++(*i)])
 	{
@@ -39,14 +47,14 @@ void	echo_with_option(char **str, int *i)
 	}
 	while (str[(*i)])
 	{
-		printf("%s", str[(*i)]);
+		print_fd(cmd, str[(*i)]);
 		if (str[(*i) + 1])
-			printf(" ");
+			print_fd(cmd, " ");
 		(*i)++;
 	}
 }
 
-void	echo_cmd(char **str)
+void	echo_cmd(t_cmd	*cmd, char **str)
 {
 	int	i;
 	int	j;
@@ -54,7 +62,7 @@ void	echo_cmd(char **str)
 	i = 1;
 	if (!str[1])
 	{
-		printf("\n");
+		print_fd(cmd, "\n");
 		return ;
 	}
 	j = check_option(str[i]);
@@ -62,13 +70,13 @@ void	echo_cmd(char **str)
 	{
 		while (str[i])
 		{
-			printf("%s", str[i]);
+			print_fd(cmd, str[i]);
 			if (str[i + 1])
-				printf(" ");
+				print_fd(cmd, " ");
 			i++;
 		}
-		printf("\n");
+		print_fd(cmd, "\n");
 	}
 	else
-		echo_with_option(str, &i);
+		echo_with_option(cmd, str, &i);
 }
--- exec/builtins/utils_builtins.c
+++ exec/builtins/utils_builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_builtins.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojamal <ojamal@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: ymenyoub <ymenyoub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 00:10:21 by ymenyoub          #+#    #+#             */
-/*   Updated: 2023/07/14 14:40:04 by ojamal           ###   ########.fr       */
+/*   Updated: 2023/07/18 02:22:47 by ymenyoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ void	execute_builtins(t_cmd *cmd, t_env_node **env)
 	if (!cmd->cmd)
 		return ;
 	if (!ft_strcmp(cmd->cmd[0], "echo"))
-		echo_cmd(cmd->cmd);
+		echo_cmd(cmd, cmd->cmd);
 	else if (!ft_strcmp(cmd->cmd[0], "env"))
 		print_env(cmd->cmd, *env);
 	else if (!ft_strcmp(cmd->cmd[0], "exit"))
